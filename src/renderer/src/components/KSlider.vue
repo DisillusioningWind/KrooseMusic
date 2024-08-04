b1b41d315de35587d35ac9fb96827c483b44bc05
Breeze <201220064@smail.nju.edu.cn>
2024-08-04 20:09:45 +0800

    优化KSlider组件的表现和逻辑

--- src/renderer/src/components/KSlider.vue
+++ src/renderer/src/components/KSlider.vue
@@ -1,16 +1,12 @@
 <template>
-  <div class="KSlider">
-    <div class="KSlider-Left"></div>
-    <div class="KSlider-Track" ref="slider" @mousedown.left.prevent="startDrag">
-      <div class="KSlider-Use"></div>
-      <div class="KSlider-Button" :cur="tooltipFormat(cur)"></div>
-      <div class="KSlider-Unuse"></div>
-    </div>
-    <div class="KSlider-Right"></div>
+  <div class="KSlider" ref="slider" @mousedown.left.prevent="startDrag">
+    <div class="Use"></div>
+    <div class="Btn" :cur="tooltipFormat(cur)"></div>
+    <div class="Unuse"></div>
   </div>
 </template>
 
-<script lang="tsx">
+<script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'KSlider',
@@ -30,7 +26,7 @@ export default defineComponent({
   setup(props, { emit }) {
     //属性
     let sliderState: 'drag' | 'slide' = 'slide'
-    const slider = ref()
+    const slider = ref<HTMLElement | null>(null)
     const min = computed(() => props.min || 0)
     const max = computed(() => props.max || 100)
     const color = computed(() => props.color || '#ffffff')
@@ -38,10 +34,16 @@ export default defineComponent({
     const tooltipFormat = computed(() => props.tooltipFormat || ((v: any) => v))
     const len = computed(() => max.value - min.value)
     const cur = ref(props.cur || 0)
-    const perUse = computed(() => cur.value / len.value * 100)
+    /** 理论占比 */
+    const perCur = computed(() => cur.value / len.value)
+    /** 实际表现占比 */
+    const perUse = computed(() => getPerUse(perCur.value))
     //工具函数
-    function getPerUseWhenDrag(clientX: number) {
-      return (clientX - slider.value.getBoundingClientRect().x) / slider.value.offsetWidth
+    function getPerUse(perCur: number) {
+      return ((slider.value!.offsetWidth - 21) * perCur) / slider.value!.offsetWidth * 100
+    }
+    function getPerCur(clientX: number) {
+      return (clientX - slider.value!.getBoundingClientRect().x - 10.5) / (slider.value!.offsetWidth - 21)
     }
     //监听外部传入的cur，仅当sliderState为slide且新旧值不等时更新内部cur
     watch(() => props.cur, (newCur, oldCur) => {
@@ -73,15 +75,15 @@ export default defineComponent({
     function startDrag(e: MouseEvent) {
       if (props.disable) return
       sliderState = 'drag'
-      cur.value = getPerUseWhenDrag(e.clientX) * len.value
+      cur.value = getPerCur(e.clientX) * len.value
     }
     function onDrag(e: MouseEvent) {
       if (sliderState !== 'drag') return
-      cur.value = getPerUseWhenDrag(e.clientX) * len.value
+      cur.value = getPerCur(e.clientX) * len.value
     }
     function endDrag(e: MouseEvent) {
       if (sliderState !== 'drag') return
-      cur.value = getPerUseWhenDrag(e.clientX) * len.value
+      cur.value = getPerCur(e.clientX) * len.value
       sliderState = 'slide'
       emit('dragCur', cur.value)
     }
@@ -112,81 +114,51 @@ $track-left-color: #ffffff40;
   flex: 1;
   display: flex;
   align-items: center;
-  .KSlider-Left {
+  &:hover {
+    .Btn {
+      border-color: $button-hover-color;
+    }
+  }
+  &:active {
+    .Btn {
+      background-color: $track-use-color;
+      &::before {
+        visibility: v-bind('tooltip ? "visible" : "hidden"');
+      }
+    }
+  }
+  .Use {
     height: $track-height;
-    width: calc($slider-height / 2);
+    width: v-bind('perUse + "%"');
     background-color: $track-use-color;
   }
-  .KSlider-Right {
+  .Unuse {
     height: $track-height;
-    width: calc($slider-height / 2);
+    flex-grow: 1;
     background-color: $track-left-color;
   }
-  .KSlider-Track {
+  .Btn {
     position: relative;
-    height: $slider-height;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    &:hover {
-      .KSlider-Button {
-        border-color: $button-hover-color;
-      }
-    }
-    &:active {
-      .KSlider-Button {
-        background-color: $track-use-color;
-        &::before {
-          visibility: v-bind('tooltip ? "visible" : "hidden"');
-        }
-      }
-    }
-    .KSlider-Use {
-      height: $track-height;
-      width: v-bind('perUse + "%"');
-      background-color: $track-use-color;
-    }
-    .KSlider-Unuse {
-      height: $track-height;
-      flex-grow: 1;
-      background-color: $track-left-color;
-    }
-    .KSlider-Button {
-      box-sizing: border-box;
-      position: absolute;
-      z-index: 1;
-      left: v-bind('perUse + "%"');
-      height: $button-width;
-      width: $button-width;
-      transform: translateX(calc($slider-height / -2));
-      border: $button-border-width solid $track-use-color;
-      border-radius: 50%;
-      margin: $button-margin-width;
-      background-color: transparent;
-      // 当前时间提示
-      &::before {
-        @include tool-tip;
-        left: -17px;
-        bottom: 28px;
-        width: 44px;
-        height: 27px;
-        content: attr(cur);
-        font-size: 13px;
-        font-weight: 400;
-        line-height: 27px;
-        text-align: center;
-        visibility: hidden;
-      }
-    }
-    &::after {
-      content: '';
-      position: absolute;
-      top: 0;
-      left: v-bind('perUse + "%"');
-      height: $slider-height;
-      width: $slider-height;
-      transform: translateX(calc($slider-height / -2));
-      background-color: v-bind('color');
+    box-sizing: border-box;
+    height: $button-width;
+    width: $button-width;
+    border: $button-border-width solid $track-use-color;
+    border-radius: 50%;
+    margin: $button-margin-width;
+    background-color: transparent;
+    // 当前时间提示
+    &::before {
+      @include tool-tip;
+      left: -17px;
+      bottom: 28px;
+      width: 44px;
+      height: 27px;
+      content: attr(cur);
+      font-size: 13px;
+      font-weight: 400;
+      line-height: 27px;
+      text-align: center;
+      visibility: hidden;
     }
   }
 }
