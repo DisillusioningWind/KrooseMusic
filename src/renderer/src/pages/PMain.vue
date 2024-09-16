a3247341c0096b2a96617f6554cc9a5e475e7a4d
Breeze <201220064@smail.nju.edu.cn>
2024-09-16 18:05:02 +0800

    style(pages): 优化背景模糊的实现方法

--- src/renderer/src/pages/PMain.vue
+++ src/renderer/src/pages/PMain.vue
@@ -1,48 +1,47 @@
 <template>
-  <div class="PMain" :class="store.showDetail?(store.musicPicURL?'Picture':'Default'):''">
-    <div class="Container" :class="store.showDetail?'Blur':''">
-      <KTitleBar />
-      <div class="KMainBar">
-        <KNavView v-show="!store.showDetail" />
-        <PDetail v-show="store.showDetail" />
-        <KListDrawer v-model="store.showDrawer" />
-      </div>
-      <KMusicBar />
+  <div class="PMain" :class="showDetail?'Blur':''">
+    <KTitleBar />
+    <div class="KMainBar">
+      <KNavView v-show="!showDetail" />
+      <PDetail v-show="showDetail" />
+      <KListDrawer v-model="showDrawer" />
     </div>
+    <KMusicBar />
   </div>
 </template>
 
 <script setup lang="ts">
 import { useStore } from '@renderer/store'
 const store = useStore()
+const { showDetail, showDrawer, musicPicURL } = storeToRefs(store)
 </script>
 
 <style scoped lang="scss">
 .PMain {
   height: 100%;
   width: 100%;
-  background-repeat: no-repeat;
-  background-position: 50% 0;
-  background-size: 100% auto;
   overflow: hidden;
-  &.Picture {
-    background-image: v-bind('"url(" + store.musicPicURL + ")"');
-  }
-  &.Default {
-    background-image: linear-gradient(150deg, #3b3b3b, #6b6b6b 20%, #ffffff);
-  }
-  >.Container {
-    height: 100%;
-    width: 100%;
-    &.Blur {
+  &.Blur {
+    background-repeat: no-repeat;
+    background-position: 50% 0;
+    background-size: 100% auto;
+    background-image: v-bind('musicPicURL?("url(" + store.musicPicURL + ")"):("linear-gradient(150deg, #3b3b3b, #6b6b6b 20%, #ffffff)")');
+    &::before {
+      content: '';
+      position: absolute;
+      height: 100%;
+      width: 100%;
       background-color: #0000005f;
       backdrop-filter: blur(40px);
     }
-    >.KMainBar {
-      height: calc(100% - 152px);
-      width: 100%;
-      display: flex;
+    >div {
+      position: relative;
     }
+  }
+  >.KMainBar {
+    height: calc(100% - 152px);
+    width: 100%;
+    display: flex;
   }
 }
 </style>
