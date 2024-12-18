0fbea484a25e3c060376a4e0035b29ca7a380c02
Breeze <201220064@smail.nju.edu.cn>
2024-12-18 20:51:30 +0800

    fix(components): 优化KLibList组件的实现逻辑

--- src/renderer/src/components/common/KLibList.vue
+++ src/renderer/src/components/common/KLibList.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="KLibList" @scroll="onScroll" >
+  <div class="KLibList" @scroll="onScroll">
     <div class="scroll-wrap">
-      <div class="item" v-for="item in itemShow" :key="item.path" :class="curPath===item.path?'select':(item['id']%2===0?'odd':'')"
+      <div class="item" v-for="item in itemShow" :key="item.path" :class="curPath === item.path?'select':(item['id']%2 === 0?'odd':'')"
         v-ctx-menu="menu" @contextmenu="onItemCtx(item)" @click="onItemClick(item)">
-        <span v-tooltip.immediate.overflow="item.name">{{ item.name + (item['ext']?item['ext']:'') }}</span>
+        <span v-tooltip.immediate.overflow="item.name">{{ item.name + (item['ext']??'') }}</span>
         <span v-if="mode === 'normal'" v-tooltip.immediate.overflow="item['artist']">{{ item['artist'] }}</span>
         <span v-if="mode === 'normal'">{{ formatTime(item['duration'], 'mm:ss') }}</span>
       </div>
@@ -17,16 +17,19 @@ import { vTooltip } from '@renderer/directives/Tooltip'
 import { vCtxMenu } from '@renderer/directives/Menu'
 const prop = defineProps<{ mode?: ListMode, items: ILibItem[], curPath?: string }>()
 const emit = defineEmits<{ select: [value: ILibItem] }>()
+const itemHeight = 40
+const itemShowNum = Math.floor(window.screen.height / itemHeight) // 虚拟加载显示的数量至少覆盖屏幕
 const _itemStart = ref(0)
 const itemStart = computed({
   get: () => _itemStart.value,
   set: (v: number) => {
     if (v < 0) { _itemStart.value = 0 }
-    else if (v > prop.items.length - 20) { _itemStart.value = prop.items.length - 20 }
+    else if (v > prop.items.length - itemShowNum) { _itemStart.value = prop.items.length - itemShowNum }
     else { _itemStart.value = v }
   }
 })
-const itemShow = computed(() => prop.items.slice(itemStart.value, itemStart.value + 20).map((v, i) => { v['id'] = itemStart.value + i; return v }))
+/** 虚拟列表只显示需要显示的item，id用于item的奇偶判断 */
+const itemShow = computed(() => prop.items.slice(itemStart.value, itemStart.value + itemShowNum).map((v, i) => { v['id'] = itemStart.value + i; return v }))
 const menu = [
   { label: '播放', action: onItemCtxPlay },
   { label: '信息', action: () => {} },
@@ -37,8 +40,8 @@ const menu = [
 let itemCtx: ILibItem | null = null
 function onScroll(e: Event) {
   const el = e.currentTarget as HTMLElement
-  itemStart.value = Math.floor(el.scrollTop / 40);
-  (el.children[0] as HTMLElement).style.transform = `translateY(${itemStart.value * 40}px)`
+  itemStart.value = Math.floor(el.scrollTop / itemHeight);
+  (el.children[0] as HTMLElement).style.transform = `translateY(${itemStart.value * itemHeight}px)`
 }
 function onItemClick(item: ILibItem) {
   emit('select', item)
@@ -62,9 +65,10 @@ function onItemCtxOpen() {
   width: 100%;
   @include k-scroll-bar;
   .scroll-wrap {
-    padding-bottom: v-bind('(items.length - itemStart - 20) * 40 + "px"');
+    // 维持虚拟列表的滚动条为原长度
+    padding-bottom: v-bind('(items.length - itemStart - itemShowNum) * itemHeight + "px"');
     .item {
-      height: 40px;
+      height: v-bind('itemHeight + "px"');
       display: flex;
       justify-content: flex-end;
       align-items: center;
