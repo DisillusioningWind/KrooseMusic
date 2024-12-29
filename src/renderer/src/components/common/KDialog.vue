7e7ae9e1555a7bf5595e96eb361070579e1efd97
Breeze <201220064@smail.nju.edu.cn>
2024-12-30 02:01:13 +0800

    feat(components): 完成KDialog通用组件的编写

--- src/renderer/src/components/common/KDialog.vue
+++ src/renderer/src/components/common/KDialog.vue
@@ -1,13 +1,44 @@
 <template>
-  <dialog class="KDialog">
-
+  <dialog ref="dialog" class="KDialog" :class="show?'show':''">
+    <slot></slot>
   </dialog>
 </template>
 
 <script setup lang="ts">
+const props = defineProps<{
+  /** 是否显示 */
+  show: boolean,
+  /** 对话框中心位置 */
+  center: number
+}>()
+const dialog = ref<HTMLDialogElement>()
 
+watch(() => props.show, v => {
+  // ::backgrop伪元素做背景遮罩，只有showModal调用才会出现；等待关闭动画结束后延时调用close
+  if (v) dialog.value?.showModal()
+  else setTimeout(() => dialog.value?.close(), 200)
+})
 </script>
 
 <style scoped lang="scss">
-
+.KDialog {
+  position: fixed;
+  top: v-bind('center - 2 + "%"');
+  left: 50%;
+  // round函数消除小数，避免像素模糊，4px是试出来的
+  transform: translate(round(-50%, 4px), round(-50%, 4px));
+  border: 2px solid #0078d7;
+  box-shadow: 0 0 20px 0 #00000030;
+  opacity: 0;
+  transition: all 0.2s;
+  &::backdrop {
+    background-color: #00000000;
+    transition: all 0.2s;
+  }
+  &.show {
+    top: v-bind('center + "%"');
+    opacity: 1;
+    &::backdrop { background-color: #00000050; }
+  }
+}
 </style>
