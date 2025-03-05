bcdb2dbb8565cf004141177e52b824ff2272ed3c
Breeze <201220064@smail.nju.edu.cn>
2025-03-05 22:48:39 +0800

    feat(api): 更换获取图片主色调的算法为中位切分以获得更好的性能和准确度
    1 更换获取图片主色调的算法为中位切分以获得更好的性能和准确度；

--- src/main/api/musicInfo.ts
+++ src/main/api/musicInfo.ts
@@ -67,27 +67,90 @@ function formatLyrics(text: string): ILyric[] {
   return lrcs
 }
 /**
- * 获取图片的主色调
+ * 使用中位切分算法获取图片的主色调
  * @param buffer 图片buffer
  * @returns 图片的主色调
  */
 async function getPictureMainColor(buffer: Uint8Array) {
-  const data = await Sharp(buffer).resize(50, 50).ensureAlpha().raw().toBuffer()
-  let max = 0
-  let maxColor = Color('#1a5d8e').hex()
-  const colors = new Map<string, number>()
-  for (let i = 0, len = data.length; i < len; i += 4) {
-    const color = Color(`rgba(${data[i]},${data[i + 1]},${data[i + 2]},${data[i + 3]})`)
-    const l = color.lightness(), s = color.saturationl(), h = color.hue()
-    if (l < 75 && l > 10 && s < 60 && s > 40 && ((h > 140 && h < 175) || h < 30 || h > 220)) {
-      const key = color.hex()
-      const count = (colors.get(key) || 0) + 1
-      colors.set(key, count)
-      if (count > max) {
-        max = count
-        maxColor = key
-      }
-    }
+  const data = await Sharp(buffer).ensureAlpha().raw().toBuffer()
+  const cube: number[][] = []
+  const pale: { rank: number, color: Color }[] = []
+  for (let i = 0; i < data.length; i += 4) {
+    cube.push([data[i], data[i + 1], data[i + 2]])
   }
-  return maxColor
+  medianCut(cube, pale)
+  // 过滤条件：亮度小于70，饱和度大于30小于80，并按rank排序
+  const res = pale.filter(c => c.color.lightness() < 70 && c.color.saturationl() > 30 && c.color.saturationl() < 80).sort((a, b) => b.rank - a.rank)
+  return res[0]?.color.hex() ?? '#1a5d8e'
+}
+/**
+ * 使用暴力遍历获取图片的主色调
+ * @param buffer 图片buffer
+ * @returns 图片的主色调
+ */
+// async function getPictureMainColorByMap(buffer: Uint8Array) {
+//   const data = await Sharp(buffer).resize(50, 50).ensureAlpha().raw().toBuffer()
+//   let maxcnt = 0
+//   let maxcol = Color('#1a5d8e').hex()
+//   const colors = new Map<string, number>()
+//   for (let i = 0; i < data.length; i += 4) {
+//     const color = Color(`rgb(${data[i]},${data[i + 1]},${data[i + 2]})`)
+//     const l = color.lightness(), s = color.saturationl(), h = color.hue()
+//     if (l < 75 && l > 10 && s < 60 && s > 40 && ((h > 140 && h < 175) || h < 30 || h > 220)) {
+//       const key = color.hex()
+//       const cnt = (colors.get(key) || 0) + 1
+//       colors.set(key, cnt)
+//       if (cnt > maxcnt) {
+//         maxcnt = cnt
+//         maxcol = key
+//       }
+//     }
+//   }
+//   return maxcol
+// }
+/**
+ * 中位切分算法
+ * @param cube 颜色立方体
+ * @param res 结果列表
+ */
+function medianCut(cube: number[][], res: { rank: number, color: Color }[]) {
+  // 限定颜色立方体的最大尺寸
+  const quality = 100
+  let maxr = cube[0][0], minr = cube[0][0]
+  let maxg = cube[0][1], ming = cube[0][1]
+  let maxb = cube[0][2], minb = cube[0][2]
+  for (let i = 1; i < cube.length; i++) {
+    maxr = Math.max(maxr, cube[i][0])
+    minr = Math.min(minr, cube[i][0])
+    maxg = Math.max(maxg, cube[i][1])
+    ming = Math.min(ming, cube[i][1])
+    maxb = Math.max(maxb, cube[i][2])
+    minb = Math.min(minb, cube[i][2])
+  }
+  const r = maxr - minr
+  const g = maxg - ming
+  const b = maxb - minb
+  // 如果颜色立方体的尺寸小于限定值，则计算平均颜色
+  if (r < quality && g < quality && b < quality) {
+    const sumcol = cube.reduce((acc, cur) => [acc[0] + cur[0], acc[1] + cur[1], acc[2] + cur[2]], [0, 0, 0])
+    const avecol = Color(`rgb(${Math.round(sumcol[0] / cube.length)},${Math.round(sumcol[1] / cube.length)},${Math.round(sumcol[2] / cube.length)})`)
+    // 以颜色立方体的体积 * 像素数量作为rank
+    res.push({ rank: cube.length * (r * g * b), color: avecol })
+  } else {
+    // 选择颜色立方体中最长的边进行切分
+    let maxrgb = 0, maxidx = 0, maxmid = 0
+    if (r > maxrgb) {
+      maxrgb = r, maxidx = 0, maxmid = (maxr + minr) / 2
+    }
+    if (g > maxrgb) {
+      maxrgb = g, maxidx = 1, maxmid = (maxg + ming) / 2
+    }
+    if (b > maxrgb) {
+      maxrgb = b, maxidx = 2, maxmid = (maxb + minb) / 2
+    }
+    const left = cube.filter(c => c[maxidx] < maxmid)
+    const right = cube.filter(c => c[maxidx] >= maxmid)
+    medianCut(left, res)
+    medianCut(right, res)
+  }
 }
